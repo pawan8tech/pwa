e092dd80b6ff4289d8344e6286dac74b85ca8a85
Pawan1 Kumawat <Pawan1.Kumawat@ril.com>
2024-05-16 12:59:22 +0530

    update the cache fetch file

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,13 @@
 const cacheName = "appV1";
 const urlsToCache = [
   "static/js/bundle.js",
-  // "manifest.json",
+  "manifest.json",
   "/index.html",
-  "/",
-  "/sw.js",
+  // "/",
+  // "/sw.js",
+
+  "/users",
+  "https://jsonplaceholder.typicode.com/users",
   "/User",
   "/About",
   // "/favicon.ico",
@@ -14,30 +17,79 @@ const urlsToCache = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(urlsToCache);
+      console.log("Caching all data");
+      return cache.addAll(urlsToCache).catch((error) => {
+        console.error("Failed to cache", error);
+      });
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
+  console.log("Fetch event for ", event.request.url);
   if (!navigator.online) {
-    //   if (event.request.url === "https://jsonplaceholder.typicode.com/users") {
-    //     event.waitUntil(
-    //       this.registration.showNotification("Internet", {
-    //         body: "Data Fetch Successfully",
-    //         icon: "/logo192.png",
-    //       })
-    //     );
-    //   }
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        if (response) {
-          return response;
-        }
+      caches
+        .match(event.request)
+        .then((response) => {
+          if (response) {
+            console.log("Found response in cache:", response);
+            return response;
+          }
 
-        let requestUrl = event.request.clone();
-        return fetch(requestUrl);
-      })
+          let requestUrl = event.request.clone();
+          return fetch(requestUrl).catch((error) => {
+            console.error("Fetch failed:", error);
+          });
+        })
+        .catch((error) => {
+          console.error("Cache match failed:", error);
+        })
     );
   }
 });
+
+// const cacheName = "appV1";
+// const urlsToCache = [
+//   "static/js/bundle.js",
+//   "manifest.json",
+//   "/index.html",
+//   "/",
+//   "/sw.js",
+//   "/User",
+//   "/About",
+//   // "/favicon.ico",
+//   // "/logo192.png",
+// ];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(cacheName).then((cache) => {
+//       console.log("cashed all data");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   if (!navigator.online) {
+//     //   if (event.request.url === "https://jsonplaceholder.typicode.com/users") {
+//     //     event.waitUntil(
+//     //       this.registration.showNotification("Internet", {
+//     //         body: "Data Fetch Successfully",
+//     //         icon: "/logo192.png",
+//     //       })
+//     //     );
+//     //   }
+//     event.respondWith(
+//       caches.match(event.request).then((response) => {
+//         if (response) {
+//           return response;
+//         }
+
+//         let requestUrl = event.request.clone();
+//         return fetch(requestUrl);
+//       })
+//     );
+//   }
+// });
